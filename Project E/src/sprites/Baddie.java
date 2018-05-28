5b094dacccd64b9b3b35f2d74d548047f8c499d8
meebee70 <ckozbial321@gmail.com>
2018-05-28 14:41:10 -0600

    QOL improvements for Baddie

--- Project E/src/sprites/Baddie.java
+++ Project E/src/sprites/Baddie.java
@@ -1,10 +1,23 @@
 package sprites;
 
+import java.awt.Image;
+
 public abstract class Baddie extends Sprite{
 
 	protected double score;
 	
 	public Baddie(double score){
+		super(50,50);
+		this.score = score;
+	}
+	
+	public Baddie(int x, int y, double score){
+		super(x,y);
+		this.score = score;
+	}
+	
+	public Baddie(int x, int y, Image img, double score){
+		super(x,y,img);
 		this.score = score;
 	}
 	
